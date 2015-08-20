e3d7e0dae5d4c840438d6a1e19f0bff7350210d1
pyohei <mukaishohei76@gmail.com>
2015-08-20 23:28:50 +0900

    add cookie treatment

--- base.py
+++ base.py
@@ -8,20 +8,24 @@ This module is base for http operation.
 from pyramid.response import Response
 from pyramid.renderers import render_to_response
 
+PASSWD = 'hogehoge'
+
 
 def hello_world(request):
     return Response('Hello %(name)s!' % request.matchdict)
 
 
-def _login(request):
-    return render_to_response(
+def _disp_login(request):
+    responce = render_to_response(
         'login.mak',
-        {'hoge': 'p', 'piyo': [1,2,3,4,5]},
+        {'hoge': 'p', 'piyo': [1, 2, 3, 4, 5]},
         request=request)
+    responce.set_cookie('hoge', 'piyopiyo')
+    return responce
 
 
-def menu(request):
-    return Response('%s' % (",".join(request.POST.keys())))
+def _disp_menu(request):
+    return Response('%s' % (",".join(request.cookies)))
 
 
 def execute():
@@ -39,9 +43,9 @@ def add_routes(config):
     config.add_route('hello', '/hello/{name}')
     config.add_view(hello_world, route_name='hello')
     config.add_route('login', '/login')
-    config.add_view(_login, route_name='login')
+    config.add_view(_disp_login, route_name='login')
     config.add_route('menu', '/menu')
-    config.add_view(menu, route_name='menu')
+    config.add_view(_disp_menu, route_name='menu')
 
 
 if __name__ == '__main__':
